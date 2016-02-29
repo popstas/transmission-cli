383db8c13543d690747969094ff14400866fc9f4
popstas <popstas@gmail.com>
2016-02-29 22:28:46 +0500

    TorrentList, add total row to table

--- src/Command/TorrentList.php
+++ src/Command/TorrentList.php
@@ -4,6 +4,7 @@ namespace Popstas\Transmission\Console\Command;
 
 use Martial\Transmission\API\Argument\Torrent;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -38,6 +39,14 @@ EOT
                 round($torrent[Torrent\Get::TOTAL_SIZE] / 1024 / 1000 / 1000, 2),
             ]);
         }
+
+        $table->addRow(new TableSeparator());
+
+        $table->addRow([
+            'Total',
+            '',
+            round($client->getTorrentsSize($torrentList) / 1024 / 1000 / 1000, 2)
+        ]);
 
         $table->render();
     }
