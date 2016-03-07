91370ae69fd475c7a123115f28abba04e1c0799e
Stanislav Popov <popstas@gmail.com>
2016-03-07 23:34:44 +0500

    refactor: change help field to rawHelp to avoid override superclass

--- src/Command/Command.php
+++ src/Command/Command.php
@@ -18,7 +18,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Command extends BaseCommand
 {
-    private $help;
+    private $rawHelp;
 
     protected function configure()
     {
@@ -140,16 +140,16 @@ class Command extends BaseCommand
 
     public function setHelp($help)
     {
-        $this->help = $help;
+        $this->rawHelp = $help;
         $help = preg_replace('/```\n(.*?)\n```/mis', '<info>$1</info>', $help);
         $help = preg_replace('/(\n|^)#+ (.*)/', '$1<question>$2</question>', $help);
         $help = preg_replace('/`([^`]*?)`/', '<comment>$1</comment>', $help);
         $help = preg_replace('/\*\*(.*?)\*\*/', '<comment>$1</comment>', $help);
         return parent::setHelp($help);
     }
-    
+
     public function getRawHelp()
     {
-        return $this->help;
+        return $this->rawHelp;
     }
 }
